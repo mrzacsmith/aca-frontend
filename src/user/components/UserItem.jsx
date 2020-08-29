c6a507aa69583f5a2f51243de760a3e7f91d0729
Zac Smith <zrs3141592@gmail.com>
2020-08-29 16:05:09 -0600

    add user and css

---
+++ src/user/components/UserItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Avatar from '../../shared/components/UIElements/Avatar.jsx'
+import Card from '../../shared/components/UIElements/Card.jsx'
+import './UserItem.css'
+
+const UserItem = (props) => {
+  return (
+    <li className="user-item">
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h2>
+              {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+            </h2>
+          </div>
+        </Link>
+      </Card>
+    </li>
+  )
+}
+
+export default UserItem
--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList.jsx'
+
 const Users = () => {
-  return <h2>Users works</h2>
+  const USERS = [
+    {
+      id: 'u1',
+      image: 'https://source.unsplash.com/random/800x600',
+      name: 'Luv Brit',
+      places: 3,
+    },
+    {
+      id: 'u2',
+      image: 'https://source.unsplash.com/random/800x600',
+      name: 'Baby Girl',
+      places: 1,
+    },
+  ]
+  return <UsersList items={USERS} />
 }
 
 export default Users
