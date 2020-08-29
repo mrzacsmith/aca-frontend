96b56995d3bc37b0cbc876e3e89f6e587253f83f
Zac Smith <zrs3141592@gmail.com>
2020-08-29 15:14:48 -0600

    set up basic router, route, switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom'
+import Users from './user/pages/Users.jsx'
+import NewPlace from './places/pages/NewPlace.jsx'
 import './App.css'
 const App = () => {
-  return <h1>React app</h1>
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
